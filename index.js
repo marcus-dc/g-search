8b6727bc7bfca6c171e5e08bd8c232fbc119d630
Marcus <techstacksph.marcus@gmail.com>
2024-05-06 13:43:08 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+
+import open from 'open';
+import { program } from 'commander';
+
+program.argument('<string...>', 'string to search').action((str, options) => {
+  const query = str.join(' ');
+  const url = `https://google.com/search?q=${encodeURIComponent(query)}`;
+  open(url);
+});
+
+program.parse();
